4c3ab3f3ba8f95945d8f38bffb22686d1114afc8
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-05-02 10:09:35 +0600

    load chefs data

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
+const chefsData = require("./data/chefs.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chefs server is running..!");
 });
 
+app.get("/chefs", (req, res) => {
+  res.send(chefsData);
+});
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Chefs server is listening on port ${port}`);
 });
